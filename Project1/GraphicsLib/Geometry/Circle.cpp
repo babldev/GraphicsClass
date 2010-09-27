0d5e89be77a332d85706879e69972ec5268bc50a
Brady Law <bradyl16@gmail.com>
2010-09-27 18:05:57 -0400

    Created Makefile

---
+++ Project1/GraphicsLib/Geometry/Circle.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Circle.cpp
+ *  Project1
+ *
+ *  Created by Brady Law on 9/27/10.
+ *  Copyright 2010 Brady Law. All rights reserved.
+ *
+ */
+
+
+#include "GraphicsLib/Geometry/Circle.h"
---
+++ Project1/GraphicsLib/Geometry/Rectangle.cpp
@@ -0,0 +1,10 @@
+/*
+ *  Rectangle.cpp
+ *  Project1
+ *
+ *  Created by Brady Law on 9/27/10.
+ *  Copyright 2010 Brady Law. All rights reserved.
+ *
+ */
+
+#include "GraphicsLib/Geometry/Rectangle.h"
